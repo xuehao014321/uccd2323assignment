7124827cc427629390506b8d38542bd0d01337a8
xuehao014321 <149219279+xuehao014321@users.noreply.github.com>
2024-08-18 15:19:51 +0800

    Update one.js

--- js/one.js
+++ js/one.js
@@ -1,8 +1,4 @@
-// JavaScript Document
-// window.onload=function(){
-// 	alert("Please click on the \"Malaysia\'s tour\" in the upper left corner to enter the homepage!");
-// }
-//标题栏下拉菜单
+
 function change(myid,mode){
 	document.getElementById(myid).style.display=mode;
 	if(mode == 'block'){//显示下拉菜单
